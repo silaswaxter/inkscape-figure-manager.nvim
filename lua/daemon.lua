37be1c3c5640819f270b6d4453cbd120b585fb96
Silas Waxter <silaswaxter@gmail.com>
2023-06-12 16:32:19 -0700

    refactor/feat(daemon): make OOP; add ensure daemon running function

--- lua/daemon.lua
+++ lua/daemon.lua
@@ -1,9 +1,26 @@
+-- A daemon process driver for posix systems.
+--
+-- Example:
+-- The following code creates a daemon that does nothing so long as there is 
+-- not already a daemon running with the same process_name
+--
+--   local Daemon = require('daemon')
+--   
+--   local function busy_loop() while true do end end
+--   
+--   local dumbyd = Daemon:new{
+--     routine = busy_loop,
+--     process_name = "dumbyd"
+--   }
+--   dumbyd:ensure_daemon()
+-- 
 local posix = require('posix')
 local posix_unistd = require('posix.unistd')
 local posix_syslog = require('posix.syslog')
+local posix_signal = require('posix.signal')
 local common_utils = require('common_utils')
 
-local daemon = {}
+local Daemon = {}
 
 local function close_all_open_file_descriptors_brute_force()
   local system_max_open_file_descriptors =
@@ -11,8 +28,49 @@ local function close_all_open_file_descriptors_brute_force()
   for i = 0, system_max_open_file_descriptors, 1 do posix_unistd.close(i); end
 end
 
--- create a daemon process which executes the daemon_function.
-function daemon.create_daemon(daemon_function)
+local function get_pid_file_name(process_name)
+  return "/run/user/" .. posix_unistd.getuid() .. "/" .. process_name .. ".pid"
+end
+
+local function is_daemon_running(process_name)
+  local pid_file = io.open(get_pid_file_name(process_name), "r")
+  if pid_file ~= nil then
+    local pid = pid_file:read("n")
+    -- check if process is running
+    if posix_signal.kill(pid, 0) ~= nil then return true end
+  end
+  return false
+end
+
+-- Writes the current process' pid to the file at /run/user/<user_id>/<process_name>.pid
+-- so that clients can check if a daemon is running.
+--
+-- See Filesystem Hierarchy Standard v3.0: https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch03s15.html
+local function overwrite_pid_file(process_name)
+  local pid_file = io.open(get_pid_file_name(process_name), "w")
+  assert(pid_file ~= nil, "pid_file was equal to nil after opening")
+  pid_file:write(posix_unistd.getpid())
+  pid_file:close()
+end
+
+-- You must initialize the following parameters when constructing a new daemon
+-- Params:
+--    o.routine      := (function) the daemon executes this function
+--    o.process_name := (string) the name of the daemon that will be used
+--                      while tracking its pid.
+function Daemon:new(o)
+  assert(o.process_name ~= nil and o.process_name ~= "" and o.routine ~= nil,
+         "You must initialize parameters constructing a new daemon.")
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+-- Create a daemon process that executes the daemon_function.
+--
+-- NOTE: Overwrites pid file meaning any running daemons with the same name
+-- will no longer be tracked.
+function Daemon:create_daemon()
   local pid, error_message = posix_unistd.fork()
   assert(pid ~= nil,
          common_utils.sanitize_error_message(error_message) .. " | 1ST FORK")
@@ -38,6 +96,8 @@ function daemon.create_daemon(daemon_function)
   end
   if pid > 0 then os.exit(0) end
 
+  overwrite_pid_file(self.process_name)
+
   posix.umask('0')
 
   local chdir_return_code
@@ -47,10 +107,15 @@ function daemon.create_daemon(daemon_function)
 
   close_all_open_file_descriptors_brute_force()
 
-  posix_syslog.openlog("nvim-inkscape-figure-managerd", posix_syslog.LOG_PID,
+  posix_syslog.openlog(self.process_name, posix_syslog.LOG_PID,
                        posix_syslog.LOG_DAEMON)
 
-  daemon_function()
+  self.routine()
 end
 
-return daemon
+-- Ensure a daemon process with the name is running
+function Daemon:ensure_daemon()
+  if not is_daemon_running(self.process_name) then self:create_daemon() end
+end
+
+return Daemon
