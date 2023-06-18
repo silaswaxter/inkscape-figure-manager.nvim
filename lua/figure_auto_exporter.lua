222168a0a40322030be818fd6f9297d37926739b
Silas Waxter <silaswaxter@gmail.com>
2023-06-18 12:25:54 -0700

    wip(figure_auto_exporter): watch directories specified by clients

--- lua/figure_auto_exporter.lua
+++ lua/figure_auto_exporter.lua
@@ -4,28 +4,61 @@
 local inotify = require('inotify')
 local LocalIpc = require('local_ipc')
 local Daemon = require('daemon')
+local posix_syslog = require('posix.syslog')
+local posix_fcntl = require('posix.fcntl')
 
 local figure_auto_exporter = {}
 
 local DAEMON_PROCESS_NAME = "inkscape_figure_managerd"
 local LOCAL_IPC_PATH = "\0" .. DAEMON_PROCESS_NAME
 
+local function busy_wait(time_s)
+  local sec = tonumber(os.clock() + time_s);
+  while (os.clock() < sec) do end
+end
+
 function figure_auto_exporter.daemon_routine(routine_params)
+  -- First, setup the socket so that clients have minimal wait time.
+  -- (recall, they cannot send messages until this endpoint is established)
   local local_ipc_receiver = LocalIpc:new{path = routine_params[1]}
+  -- make socket non-blocking
+  local socket_flags = posix_fcntl.fcntl(local_ipc_receiver.socket,
+                                         posix_fcntl.F_GETFL)
+  socket_flags = socket_flags | posix_fcntl.O_NONBLOCK
+  assert(posix_fcntl.fcntl(local_ipc_receiver.socket, posix_fcntl.F_SETFL,
+                           socket_flags))
   local_ipc_receiver:bind_socket()
+
+  local watch_descriptors = {}
+  local inotify_handle = inotify.init({blocking = false})
+
   while true do
-    local_ipc_receiver:syslog_datagram_poll(true)
+    local message = local_ipc_receiver:read_datagram_poll(false)
+    if message ~= nil then
+      local _, _, dir_to_watch = message:find(".-watch:(.*)")
+
+      if dir_to_watch ~= nil then
+        posix_syslog.syslog(posix_syslog.LOG_INFO, "watching:'" .. dir_to_watch .. "'")
+
+        local wd = inotify_handle:addwatch(dir_to_watch, inotify.IN_CREATE,
+                                           inotify.IN_MODIFY)
+        watch_descriptors[wd] = dir_to_watch
+      end
+    end
+    for event in inotify_handle:events() do
+      local figure_extension_index = string.find(event.name, '.svg')
+      if figure_extension_index ~= nil then
+        local file_absolute_path = watch_descriptors[event.wd] .. "/" ..
+                                     event.name
+        posix_syslog.syslog(posix_syslog.LOG_INFO,
+                            file_absolute_path .. " created or modified")
+      end
+    end
   end
 end
 
-local function busy_wait (time_s)
-    local sec = tonumber(os.clock() + time_s);
-    while (os.clock() < sec) do
-    end
-end
-
 function figure_auto_exporter.client_add_watch_location(path_to_watch)
-  local daemon = Daemon:new {
+  local daemon = Daemon:new{
     routine = figure_auto_exporter.daemon_routine,
     process_name = DAEMON_PROCESS_NAME,
     routine_params = {LOCAL_IPC_PATH}
@@ -33,7 +66,7 @@ function figure_auto_exporter.client_add_watch_location(path_to_watch)
   daemon:ensure_daemon()
 
   -- when a daemon is created, need to wait for it to open the socket
-  busy_wait(0.4)
+  busy_wait(1)
   local local_ipc_sender = LocalIpc:new{path = LOCAL_IPC_PATH}
   local_ipc_sender:send_datagram("watch:" .. path_to_watch)
 end
--- lua/test_figure_auto_exporter.lua
+++ lua/test_figure_auto_exporter.lua
@@ -1,3 +1,3 @@
 local FigureAutoExporter = require('figure_auto_exporter')
 
-FigureAutoExporter.client_add_watch_location("/home/silas")
+FigureAutoExporter.client_add_watch_location(arg[1] or "/home/silas")
